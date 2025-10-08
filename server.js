b37b8b45e701c00dced762b865fa67066cfb49c0
brunocarvsilva99-design <brunocarvsilva99@gmail.com>
2025-10-08 10:30:13 -0300

    conexão com MongoDB Atlas via variável de ambiente

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/pesquisaod', { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl = process.env.MONGO_URL;
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('Conectado ao MongoDB Atlas!');
+    // Inicie o servidor só após conectar ao banco
+    app.listen(3001, () => {
+      console.log('Servidor rodando na porta 3001');
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err);
+  });
 
 const PesquisaSchema = new mongoose.Schema({}, { strict: false });
 const Pesquisa = mongoose.model('Pesquisa', PesquisaSchema);
@@ -20,8 +31,4 @@ app.post('/pesquisas', async (req, res) => {
 app.get('/pesquisas', async (req, res) => {
   const pesquisas = await Pesquisa.find();
   res.json(pesquisas);
-});
-
-app.listen(3001, () => {
-  console.log('Servidor rodando na porta 3001');
 });
